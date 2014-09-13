e11a8af58425a0707803e3cd0f6fff9245b970ac
Magda Gumularz <mmgumularz@gmail.com>
2014-09-13 19:42:36 +0200

    categories spec passes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
   end
 
   protect_from_forgery with: :exception
+
+  add_flash_types :error
+
+  def authenticate_admin
+   unless current_user.admin?
+    redirect_to new_user_session_path
+   end
+  end
+
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
+  before_action :authenticate_admin, only: [:update, :create, :edit, :new]
   
   expose(:categories)
   expose(:category)
