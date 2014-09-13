7301537954e39e3178401e4dad56550955cb460f
Magda Gumularz <mmgumularz@gmail.com>
2014-09-13 19:44:58 +0200

    products spec passes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,19 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :title
+  validates_presence_of :price
+  validates_presence_of :description
+
+  validates_format_of :price, with: /\A([0-9]*).([0-9]{0,2})\z/i
+
+  def average_rating
+   av = 0
+   reviews.each do |r|
+    av += r.rating
+   end
+   a = av.to_f / reviews.size
+  end
+
 end
