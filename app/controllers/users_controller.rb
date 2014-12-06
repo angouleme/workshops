5da77680b411bba74100710e2790a1c09a37bcd4
Magda Gumularz <mmgumularz@gmail.com>
2014-12-06 12:55:32 +0100

    checking the repo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,8 @@ class UsersController < ApplicationController
   @reviews = Review.where(:user_id => @user.id).limit(5)
   @products = Product.where(:user_id => @user.id)
  end
+
+ private
+ def test_method
+ end
 end
